663b20038048cf2e5e0c7dc1a75683fba5052cb0
xu <spiridonova.kseniya@gmail.com>
2023-08-14 17:03:21 +0600

    fix: fix state in app.tsx and fix local storage clean in header.tsx

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -11,7 +11,7 @@ export default function Header(): JSX.Element {
 
   const handleLogout = (): void => {
 
-    localStorage.removeItem('user');
+    localStorage.removeItem('currentUser');
     setUser(anonUser);
 
   };
