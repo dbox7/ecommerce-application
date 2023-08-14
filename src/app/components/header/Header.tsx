57a739f9e307a6e9806d621cfdefe1e64e8f36a8
GEKKO-ops <artem@mikolyuk.com>
2023-08-14 12:26:35 +0300

    fix: localStorage item name

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -11,7 +11,7 @@ export default function Header(): JSX.Element {
 
   const handleLogout = (): void => {
 
-    localStorage.removeItem('user');
+    localStorage.removeItem('currentUser');
     setUser(anonUser);
 
   };
@@ -24,7 +24,7 @@ export default function Header(): JSX.Element {
           <Link to="/">Main</Link>
         </div>
         <nav className="nav">
-          {!user.id ?
+          {!user ?
             <ul className="menu-list">
               <li className="menu-item">
                 <Link to="/login">Log in</Link>
