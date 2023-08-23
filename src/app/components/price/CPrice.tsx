ab53e14e1e1702371c70f427d7f515b877b88a3e
Dmitry Korobka <dbox.insight@gmail.com>
2023-08-23 23:47:18 +0300

    feat: create Price component

---
+++ src/app/components/price/CPrice.tsx
@@ -0,0 +1,41 @@
+import { Price } from '@commercetools/platform-sdk';
+import { FC } from 'react';
+
+import './CPrice.css';
+
+interface IPriceProps {
+  price: Price,
+  isMini?: boolean
+}
+
+const CPrice:FC<IPriceProps> = ({ price, isMini = false }) => {  
+
+  return (
+
+    price.discounted ?
+      <div className="price">
+        <del className="crossed">
+          ${price.value.centAmount / 100}
+        </del>
+        <div className="new_price">
+          ${price.discounted.value.centAmount / 100}
+        </div>
+      </div>
+      :
+      <div className="price">
+        <div className="crossed">
+          ${price.value.centAmount / 100}
+        </div>
+        <div className="new_price">
+          ${price.value.centAmount / 100}
+        </div>
+      </div>
+      // <div className="price">
+      //   ${price.value.centAmount / 100}
+      // </div>
+
+  );
+
+};
+ 
+export default CPrice;
--- src/app/pages/product/ProductPage.tsx
+++ src/app/pages/product/ProductPage.tsx
@@ -2,13 +2,18 @@ import { useContext, useEffect, useState } from 'react';
 import { apiAnonRoot } from '../../ctp';
 import { GlobalContext } from '../../store/GlobalContext';
 import { useParams } from 'react-router-dom';
-import { ProductVariant } from '@commercetools/platform-sdk';
+import { ProductProjection } from '@commercetools/platform-sdk';
 import CViewImage from '../../components/viewImage/CViewImage';
+import CButton from '../../components/button/CButton';
+
+import './ProductPage.css';
+import CPrice from '../../components/price/CPrice';
+
 
 function GetProduct(id: string) {
 
   const [globalStore] = useContext(GlobalContext);
-  const [product, setProduct] = useState<ProductVariant>();
+  const [product, setProduct] = useState<ProductProjection>();
   const [error, setError] = useState<string>('');
 
   useEffect(() => {
@@ -29,11 +34,11 @@ function GetProduct(id: string) {
       .withId({ID: id as string})
       .get().execute().then(res => {
         
-        setProduct(res.body.masterVariant);
+        setProduct(res.body);
         
       }).catch(err => {
         
-        setError(err);
+        setError(err);        
         
       });
 
@@ -48,16 +53,34 @@ export function ProductPage() {
  
   const props = useParams();
   
-  const product = GetProduct(props.id!).product;  
-  
-  return ( 
-    <div>
-      {product &&
-      <CViewImage 
-        images={product?.images!}
-        color={product?.attributes![0].value.key}
-      />}
-    </div>
+  const product = GetProduct(props.id!).product;
+  const productData = product?.masterVariant;  
+
+  return (
+    product ? 
+      <div className="product_page">
+        <CViewImage 
+          images={productData?.images!}
+          color={productData?.attributes![0].value.key}
+        />
+        <div className="product_info">
+          <div className="product_info-text">
+            <div className="product_title">
+              {product?.name.en}
+            </div>
+            <div className="product_description">
+              {product?.description!.en}
+            </div>
+          </div>
+          <CPrice price={productData?.prices![0]!} />
+          <CButton 
+            value="Add to cart +"
+            type="button"
+          />
+        </div>
+      </div>
+      :
+      <div>Loading...</div>
   );
 
 };
