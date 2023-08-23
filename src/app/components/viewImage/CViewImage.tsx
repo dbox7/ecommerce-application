e99c1ab0123a22359f17292ea478e720405baf6a
Dmitry Korobka <dbox.insight@gmail.com>
2023-08-23 16:12:16 +0300

    feat: create viewImage component

---
+++ src/app/components/viewImage/CViewImage.tsx
@@ -0,0 +1,20 @@
+
+
+const CViewImage = () => {
+
+
+
+  return ( 
+    <div className="viewImage">
+      <div className="viewImage_main">
+
+      </div>
+      <div className="viewImage_variants">
+
+      </div>
+    </div>
+  );
+
+};
+ 
+export default CViewImage;
