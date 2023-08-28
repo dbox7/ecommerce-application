fee80d7c2bbee8d76657d203e5082f2ff47415db
xu <spiridonova.kseniya@gmail.com>
2023-08-28 18:06:57 +0600

    feat: add footer with links to about us page, contacts page and social media

---
+++ src/app/pages/about/AboutUsPage.tsx
@@ -0,0 +1,36 @@
+import './AboutUsPage.css';
+
+export function AboutUsPage() {
+
+  return (
+
+    <div className="container">
+      <div className="content">
+        <h1 className="about-page-title">About Us</h1>
+        <div className="about-page-text">
+          Hey there, username!<br/>
+
+          We're a team of young web developers, united by a common goal: to create 
+          an educational project for RS-School. This project is not only our 
+          first experience of collaboration but also our first foray into React development.<br/>
+
+          Inspired by cutting-edge technologies, we also decided to utilize 
+          artificial intelligence tools. With the help of ChatGPT, 
+          we crafted informative product descriptions to enrich our content. 
+          And speaking of Midjorney – it's our idol when it comes to visual creation, 
+          adding that "wow" factor to our products.<br/>
+
+          We're open to feedback and value diverse perspectives. 
+          If you have suggestions or observations, let us know. 
+          Additionally, if you share our drive and propose collaboration, 
+          drop us a line through the "Contact Us" page.<br/>
+          
+          Best regards,
+          The "JS do IT" Team
+        </div>
+      </div>
+    </div>
+
+  );
+
+}
---
+++ src/app/pages/contacts/ContactsPage.tsx
@@ -0,0 +1,15 @@
+import './ContactsPage.css';
+
+export function ContactsPage() {
+
+  return (
+
+    <div className="container">
+      <div className="content">
+        <h1 className="contacts-page-title">contacts will be here</h1>
+      </div>
+    </div>
+
+  );
+  
+}
