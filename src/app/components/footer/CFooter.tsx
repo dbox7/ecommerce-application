1595e5742f41c8050aa0760fa2db2bbc0c941d5a
xu <spiridonova.kseniya@gmail.com>
2023-08-29 00:40:16 +0600

    fix: add media query for footer

--- src/app/components/footer/CFooter.tsx
+++ src/app/components/footer/CFooter.tsx
@@ -12,13 +12,13 @@ export function Footer() {
       <div className="footer-container">
         <div className="footer-outer-links">
           <div className="footer-icons">
-            <div className="footer-logo">
-              <Link to="/" className="link">SNEAKERS STORE</Link>
-            </div>
             <Link to="https://facebook.com/" className="link" target="_blank"><CiFacebook className="footer-icon"/></Link>
             <Link to="https://www.instagram.com/" className="link" target="_blank"><CiInstagram className="footer-icon"/></Link>
             <Link to="https://www.youtube.com/" className="link" target="_blank"><CiYoutube className="footer-icon"/></Link>
           </div>
+        </div>
+        <div className="footer-logo">
+          <Link to="/" className="link">SNEAKERS STORE</Link>
         </div> 
         <div className="footer-inner-links">
           <NavLink to="/about" className="footer-link">About Us</NavLink>
