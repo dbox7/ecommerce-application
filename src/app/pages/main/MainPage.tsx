2f6f8c883e4da84fa1593194db3b6d7b218b0659
xu <spiridonova.kseniya@gmail.com>
2023-08-31 23:28:03 +0600

    feat: add promo component for registered users

--- src/app/pages/main/MainPage.tsx
+++ src/app/pages/main/MainPage.tsx
@@ -1,11 +1,29 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../../store/GlobalContext';
+
+import { CPromo } from '../../components/promo/CPromo';
+
 import './MainPage.css';
 
 export const MainPage = () => {
 
+  const [globalStore] = useContext(GlobalContext);
+
   return (
 
-    <div className="poster-wrap"></div>
-    
+    <>
+      { globalStore.currentUser.id ? 
+
+        <>
+          <div className="poster-wrap"></div>
+          <CPromo/>
+        </>
+            
+        :
+        <div className="poster-wrap"></div>
+      }
+    </>
+
   );
 
 };
