662c6d3e0b7db1c06a887ff2b4fdd9df11da70f4
xu <spiridonova.kseniya@gmail.com>
2023-09-04 21:47:52 +0600

    feat: adaptive for sort by

--- src/app/components/products/sort/CSortProducts.tsx
+++ src/app/components/products/sort/CSortProducts.tsx
@@ -15,12 +15,14 @@ export function CSortProducts({filters, setFilters} : ISortProductsProps) {
 
     <div className="sort-by-container">
       <label className="sort-by-label">Sort by:</label>
-      <select className="sort-by-select" onChange={sortItems}>
-        <option value="name.en asc">Name Ascending Sort</option>
-        <option value="name.en desc">Name Descending Sort</option>
-        <option value="price asc">Price Ascending Sort</option>
-        <option value="price desc">Price Descending Sort</option>
-      </select>
+      <div className="select-wrapper">
+        <select className="sort-by-select" onChange={sortItems}>
+          <option value="name.en asc">Name Ascending</option>
+          <option value="name.en desc">Name Descending</option>
+          <option value="price asc">Price Ascending</option>
+          <option value="price desc">Price Descending</option>
+        </select>
+      </div>
     </div>
 
   );
