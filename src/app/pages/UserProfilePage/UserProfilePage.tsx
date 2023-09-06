4bd25c6c29db88127ce238a1f846c1caca7ddcf2
xu <spiridonova.kseniya@gmail.com>
2023-09-07 00:15:12 +0600

    feat: add redirect from profile to login

--- src/app/pages/UserProfilePage/UserProfilePage.tsx
+++ src/app/pages/UserProfilePage/UserProfilePage.tsx
@@ -1,6 +1,24 @@
+import { useContext, useEffect } from 'react';
+import { GlobalContext } from '../../store/GlobalContext';
+import { useNavigate } from 'react-router-dom';
+
 import CUserProfileForm from '../../components/userProfileForm/CUserProfileForm';
 
+
 export const UserProfilePage = () => {
+  
+  const [globalStore] = useContext(GlobalContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+
+    if (!globalStore.currentUser.id) {
+
+      navigate('/login');
+  
+    }
+
+  });
 
   return (
     <CUserProfileForm />
