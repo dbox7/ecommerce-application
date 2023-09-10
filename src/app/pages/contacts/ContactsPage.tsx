23f67a626d4eb9f59d0a19470c11c89f7cc657c8
xu <spiridonova.kseniya@gmail.com>
2023-09-11 01:39:51 +0600

    fix: fix company name and description

--- src/app/pages/contacts/ContactsPage.tsx
+++ src/app/pages/contacts/ContactsPage.tsx
@@ -13,19 +13,20 @@ export const ContactsPage = () => {
           <iframe title="google-map" src={GOOGLE_MAP} style={{border:0}} allowFullScreen loading="lazy"></iframe>
         </div>
         <div className="contacts-page-text">
-          <p><b>Company Name:</b> JS DO SNEAKERS, LLC</p>
+          <p><b>Company Name:</b> DUST SNEAKERS, LLC</p>
           <p><b>Address:</b> 123 Main Street of Nothing, AZ 85001 United States</p>
           <p><b>Phone:</b> +1 (555) 555-5555</p>
-          <p><b>Email:</b> info@jsdosneakers.com</p>
-          <p><b>Website:</b> www.jsdosneakers.com</p>
-          <p><b>Company Description:</b> JS do Sneakers - your reliable partner in the world of sneakers and athletic footwear. 
-            We offer a wide range of sneakers from leading brands to meet all your athletic needs. 
-            Our stores are located in the town of Nothing, Arizona, and we deliver products nationwide. 
-            Whether you're into running, basketball, or any other sport, Sports Sneakers has the perfect pair of sneakers for you. 
-            Additionally, some of our models are covered in the dust of legendary abandoned roads to add a unique charm to your style.
+          <p><b>Email:</b> info@dust-sneakers.com</p>
+          <p><b>Website:</b> www.dust-sneakers.com</p>
+          <p><b>Company Description:</b> DUST Sneakers - your trusted companion in the realm of sneakers and athletic footwear. 
+            We present an extensive selection of sneakers from top-tier brands to cater to all your athletic requirements. 
+            Our establishments are nestled within the quaint township of Nothing, Arizona, while our nationwide delivery 
+            ensures your convenience. Whether you're passionate about running, basketball, or any other sport, DUST Sneakers 
+            offers the ideal pair of sneakers for your pursuits. Moreover, certain models within our collection are adorned 
+            with the venerable dust of legendary forsaken pathways, infusing your style with a distinctive allure.
           </p>
           <p><b>Operating Hours:</b><br/>Monday - Friday: 9:00 AM - 6:00 PM<br/>Saturday: 10:00 AM - 4:00 PM<br/>Sunday: Closed</p>
-          <p><b>Social Media:</b><br/>Facebook: @jsdosneakers<br/>Instagram: @jsdosneakers<br/>Twitter: @jsdosneakers</p>
+          <p><b>Social Media:</b><br/>Facebook: @dustsneakers<br/>Instagram: @dustsneakers<br/>Twitter: @dustsneakers</p>
           <p>Please note that these are fictional details and are not associated with any real company.</p>
         </div>
       </div>
