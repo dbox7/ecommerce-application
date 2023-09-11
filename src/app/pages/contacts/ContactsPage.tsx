de44b32c1944a58caaee20287e485ecc2e534019
xu <spiridonova.kseniya@gmail.com>
2023-09-11 15:38:20 +0600

    feat: add BEM nootation

--- src/app/pages/contacts/ContactsPage.tsx
+++ src/app/pages/contacts/ContactsPage.tsx
@@ -6,13 +6,13 @@ export const ContactsPage = () => {
 
   return (
 
-    <div className="contacts-page">
-      <h1 className="contacts-page-title">Contacts</h1>
-      <div className="contacts-page-content">
-        <div className="contacts-page-map">
-          <iframe title="google-map" src={GOOGLE_MAP} style={{border:0}} allowFullScreen loading="lazy"></iframe>
+    <div className="contacts">
+      <h1 className="contacts__title">Contacts</h1>
+      <div className="contacts__content">
+        <div className="contacts__content__map">
+          <iframe title="contacts__content__map google" src={GOOGLE_MAP} style={{border:0}} allowFullScreen loading="lazy"></iframe>
         </div>
-        <div className="contacts-page-text">
+        <div className="contacts__content__text">
           <p><b>Company Name:</b> DUST SNEAKERS, LLC</p>
           <p><b>Address:</b> 123 Main Street of Nothing, AZ 85001 United States</p>
           <p><b>Phone:</b> +1 (555) 555-5555</p>
