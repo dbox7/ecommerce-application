e21adaa6effd2d92c509083dea688fcda7581882
dbox7 <dbox.insight@gmail.com>
2023-09-19 11:47:38 +0300

    fix: display promocode without login

--- src/app/components/products/list/CProductList.tsx
+++ src/app/components/products/list/CProductList.tsx
@@ -21,9 +21,9 @@ export const CProductList = memo(() => {
   const [items, setItems] = useState<ProductProjection[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const getSlice = (page: number, arr = products) => {    
+  const getSlice = (page: number, arr = products) => {   
     
-    const res = arr.slice(page * 5, (page + 1) * 5);
+    const res = arr.slice(page * 5, (page + 1) * 5);    
 
     if (res.length) {
 
@@ -35,7 +35,13 @@ export const CProductList = memo(() => {
 
   };
 
-  const fetchData = useCallback(() => {   
+  useEffect(() => {
+
+    setItems(getSlice(0));
+
+  }, [products]);
+
+  const fetchData = useCallback(() => {    
 
     if (isLoading) return;
     
@@ -70,12 +76,6 @@ export const CProductList = memo(() => {
     };
 
   }, [fetchData]);
-
-  useEffect(() => {
-    
-    setItems(getSlice(0));
-
-  }, [products]);
 
   return (
     <div className="product__wrap">
--- src/app/pages/main/MainPage.tsx
+++ src/app/pages/main/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useTypedSelector } from '../../store/hooks/useTypedSelector';
+// import { useTypedSelector } from '../../store/hooks/useTypedSelector';
 
 import { CPromo25 } from '../../components/promo/discount25/CPromo25';
 import { CPromo10 } from '../../components/promo/discount10/CPromo10';
@@ -8,27 +8,18 @@ import './MainPage.css';
 
 export const MainPage = () => {
 
-  const { currentUser } = useTypedSelector(state => state.user);
+  // const { currentUser } = useTypedSelector(state => state.user);
 
   return (
 
-    <>
-      { currentUser.id ? 
+    <div className="main-wrap">
+      <div className="poster-wrap"/>
+      <div className="promo-wrap">
+        <CPromo25/>
+        {/* <CPromo10/> */}
+      </div>
 
-        <div className="main-wrap">
-          <div className="promo-wrap">
-            <CPromo25/>
-            <CPromo10/>
-          </div>
-          <div className="poster-wrap"/>
-
-        </div>
-
-        
-        :
-        <div className="poster-wrap-full"/>
-      }
-    </>
+    </div>
 
   );
 
