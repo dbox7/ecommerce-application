0ae81d20e44c68b79e7f51bbb781224e9c7a9cab
Dmitry Korobka <dbox.insight@gmail.com>
2023-09-25 14:18:30 +0300

    fix: add dependences from react lib

--- src/app/components/filters/CFiltersMenu.tsx
+++ src/app/components/filters/CFiltersMenu.tsx
@@ -1,7 +1,7 @@
 import useMultiRange from '../../services/input/useMultiRange';
 import { ProductProjection } from '@commercetools/platform-sdk';
 import { getSizeArray } from '../../utils/usefullFuncs';
-import { useEffect, useState, memo } from 'react';
+import { useEffect, useState, memo, FC } from 'react';
 import { useResize } from '../../services/useResize';
 import { useTypedSelector } from '../../store/hooks/useTypedSelector';
 
